44f02f9cc8b2963284eb30768cb4b790cfaf43ff
= <nncl@live.com>
2018-08-01 20:57:52 -0300

    Adding custom env variable from heroku

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express'),
     router = express.Router(),
-    env = require('../env')
+    env = process.env.TITLE ? process.env.TITLE : require('../env')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    res.render('index', {title: env.settings.title});
+    res.render('index', {title: env.settings ? env.settings.title : env});
 });
 
 module.exports = router;
