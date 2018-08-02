ea6f2ce6bd99d457b94a5dc097f56ee3ee952e79
= <nncl@live.com>
2018-08-01 21:40:01 -0300

    Enabling access and denying default browser CORS

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ const app = express()
 
 require('./config/database')
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, PATCH, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
+app.options('*', (req, res) => {
+    res.sendStatus(200);
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
