4ed518622aaaff446874680e18425e994a9cbb33
= <nncl@live.com>
2018-08-02 16:10:54 -0300

    POST to New Contact on Firebase

--- env.example.js
+++ env.example.js
@@ -2,5 +2,6 @@ module.exports.settings = {
     "TITLE" : "STRV Address Book API",
     "MONGO_URI" : "", // should be on heroku env variables
     "SECRET" : "", // should be on heroku env variables
-    "EXPIRES_IN" : 0 // should be on heroku env variables - milliseconds
+    "EXPIRES_IN" : 0, // should be on heroku env variables - milliseconds
+    "FIREBASE_DATABASE_URI" : "" // should be on heroku env variables - milliseconds
 }
--- routes/api.js
+++ routes/api.js
@@ -2,5 +2,6 @@ const express = require('express'),
     app = express()
 
 app.use('/users', require('../modules/Users'))
+app.use('/contacts', require('../modules/Contacts'))
 
 module.exports = app
